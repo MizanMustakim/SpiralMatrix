f7447ea3bd7b41681c84eb603ad540acf353273a
Md Mizanur Rahman Mustakim <56040932+MizanMustakim@users.noreply.github.com>
2020-07-04 01:05:17 +0600

    Update spiralMatrixSourceCode.py

--- spiralMatrixSourceCode.py
+++ spiralMatrixSourceCode.py
@@ -3,13 +3,23 @@ matrix = [[1, 2, 3, 4, 5],
           [11, 12, 13, 14, 15],
           [16, 17, 18, 19, 20]]
 
-i = 0
-while i < len(matrix):
-    if i % 2 == 0:
-        for rows in matrix[i]:
-            print(rows)
-    elif i % 2 == 1:
-        matrix[i].sort(reverse=True)
-        for row in matrix[i]:
-            print(row)
-    i += 1
+# print rows between lo_x..hi_x and columns between lo_y..hi_y (inclusive)
+lo_x = lo_y = 0
+hi_x = len(matrix[0]) - 1
+hi_y = len(matrix) - 1
+
+while lo_x <= hi_x and lo_y <= hi_y:
+    for x in range(lo_x, hi_x+1):
+        print(matrix[lo_y][x])
+    lo_y += 1
+    for y in range(lo_y, hi_y + 1):
+        print(matrix[y][hi_x])
+    hi_x -= 1
+    if not (lo_x <= hi_x and lo_y <= hi_y):
+        break
+    for x in reversed(range(lo_x, hi_x + 1)):
+        print(matrix[hi_y][x])
+    hi_y -= 1
+    for y in reversed(range(lo_y, hi_y + 1)):
+        print(matrix[y][lo_x])
+    lo_x += 1
